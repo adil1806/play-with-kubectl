41d0eed16ab1a0ee9b3b53041e0e32a387b495c2
Khan adil <khanabdullahadil@gmail.com>
2020-10-09 06:18:54 +0000

    initial commit for k8s mini project

--- awesome-k8s-mini-project/src/main/java/com/example/helloworld/HelloworldApplication.java
+++ awesome-k8s-mini-project/src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloworldApplication {
-	@Value("${TARGET:World}")
+	@Value("${TARGET:Kubernetes}")
         String target;
 
         @RestController
