2a7707ad9ad76fdd7b2e635e473a5a7de546fbb5
KHAN ADIL <36574088+adil1806@users.noreply.github.com>
2020-10-15 10:00:45 +0530

    Update kubernetes-worker-node.sh

--- kubernetes-installation/kubernetes-worker-node.sh
+++ kubernetes-installation/kubernetes-worker-node.sh
@@ -7,8 +7,13 @@
 ###########################################################################################################################
 
 #!/bin/bash
-apt-get update
-apt-get install -y apt-transport-https
+echo "[TASK 1] Updating system"
+apt-get update >/dev/null 2>&1
+
+echo "[TASK 2] Install Transport HTTPS"
+apt-get install -y apt-transport-https >/dev/null 2>&1
+
+echo "[TASK 3] Adding key for Docker"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -16,9 +21,10 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable
 EOF
 
-apt-get update
+apt-get update >/dev/null 2>&1
 
-apt-get install -y docker.io
+echo "[TASK 4] Installing Docker"
+apt-get install -y docker.io >/dev/null 2>&1
 
 systemctl stop docker
 modprobe overlay
@@ -26,10 +32,11 @@ echo '{"storage-driver": "overlay2"}' > /etc/docker/daemon.json
 rm -rf /var/lib/docker/*
 systemctl start docker
 
+echo "[TASK 5] Installing Tools for K8s"
 # Install kubernetes components!
 apt-get install -y \
         kubelet \
         kubeadm \
         kubernetes-cni \
-        nfs-common
+        nfs-common >/dev/null 2>&1
 # nfs-common util for nfs dynamic provisioning
