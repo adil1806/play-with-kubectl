a98a57500809e4e7132cfea87bfe81fea46119ce
KHAN ADIL <36574088+adil1806@users.noreply.github.com>
2020-12-09 15:40:32 +0530

    Update kubernetes-worker-node.sh

--- kubernetes-installation/kubernetes-worker-node.sh
+++ kubernetes-installation/kubernetes-worker-node.sh
@@ -7,13 +7,13 @@
 ###########################################################################################################################
 
 #!/bin/bash
-echo "[TASK 1] Updating system"
+echo "[TASK 1] System Update"
 apt-get update >/dev/null 2>&1
 
 echo "[TASK 2] Install Transport HTTPS"
 apt-get install -y apt-transport-https >/dev/null 2>&1
 
-echo "[TASK 3] Adding key for Docker"
+echo "[TASK 3] Adding Required Key for Docker"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -32,11 +32,29 @@ echo '{"storage-driver": "overlay2"}' > /etc/docker/daemon.json
 rm -rf /var/lib/docker/*
 systemctl start docker
 
-echo "[TASK 5] Installing Tools for K8s"
+echo "[TASK 5] Installing Additional Tools for K8s"
 # Install kubernetes components!
 apt-get install -y \
         kubelet \
         kubeadm \
-        kubernetes-cni >/dev/null 2>&1
-       # nfs-common 
+        kubernetes-cni \
+        nfs-common >/dev/null 2>&1
 # nfs-common util for nfs dynamic provisioning
+if [[ $(hostname) =~ .*master.* ]]
+then
+   echo "[TASK 6] Setting up Kubernetes Cluster"
+   kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all >> /root/kubeinit.log 2>&1
+   
+   echo "[TASK 7] Copying kubeconfig file to root user under .kube directory"
+   mkdir /root/.kube
+   cp /etc/kubernetes/admin.conf /root/.kube/config
+   
+   echo "[TASK 8] Deploying flannel network"
+   kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml > /dev/null 2>&1
+   
+   echo "[TASK 9] Generating & save cluster join command to /jointoken.txt"
+   joinToken=$(kubeadm token create --print-join-command 2>/dev/null) 
+   echo "$joinToken --ignore-preflight-errors=all" > /jointoken.txt
+fi
+
+
