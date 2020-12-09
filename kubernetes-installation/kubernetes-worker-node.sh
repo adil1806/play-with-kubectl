8b3c31083a505aa627abe5cd01dc2adacbee67a8
KHAN ADIL <36574088+adil1806@users.noreply.github.com>
2020-12-09 16:02:39 +0530

    updated with cluster setup

--- kubernetes-installation/kubernetes-worker-node.sh
+++ kubernetes-installation/kubernetes-worker-node.sh
@@ -1,10 +1,11 @@
-#######################################################################
-#### Installation of kubeadm and docker for kubernetes worker node ####
-################# system compatible Ubuntu:18.04.5 LTS (Bionic Beaver) #######################
-####################   Maintainer: Adil Khan  #########################
-############### To Run this script ####################################
-### curl -s https://raw.githubusercontent.com/adil1806/play-with-kubectl/master/kubernetes-worker-node.sh | bash ####
-###########################################################################################################################
+############################################################# PLAY WITH KUBECTL #############################################################################
+#       Installation of Docker and all required tools for kubernetes and setting up kubeadm cluster if node hostname includes master keyword in it's hostname.
+#       Else this script will install docker and required tools for kubernetes only.
+#       system compatible: Ubuntu:18.04.5 LTS (Bionic Beaver) and 20.04.1 LTS (Focal Fossa).     
+#       Maintainer: Adil Abdullah Khan  
+#       To Run this script:
+#       curl  https://raw.githubusercontent.com/adil1806/play-with-kubectl/master/kubernetes-worker-node.sh | bash
+#############################################################################################################################################################
 
 #!/bin/bash
 echo "[TASK 1] System Update"
@@ -14,13 +15,12 @@ echo "[TASK 2] Install Transport HTTPS"
 apt-get install -y apt-transport-https >/dev/null 2>&1
 
 echo "[TASK 3] Adding Required Key for Docker"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - >/dev/null 2>&1
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable
 EOF
-
 apt-get update >/dev/null 2>&1
 
 echo "[TASK 4] Installing Docker"
@@ -40,6 +40,8 @@ apt-get install -y \
         kubernetes-cni \
         nfs-common >/dev/null 2>&1
 # nfs-common util for nfs dynamic provisioning
+
+# kubernets master node must have "Master" keyword as hostname
 if [[ $(hostname) =~ .*master.* ]]
 then
    echo "[TASK 6] Setting up Kubernetes Cluster"
@@ -55,6 +57,5 @@ then
    echo "[TASK 9] Generating & save cluster join command to /jointoken.txt"
    joinToken=$(kubeadm token create --print-join-command 2>/dev/null) 
    echo "$joinToken --ignore-preflight-errors=all" > /jointoken.txt
+   # Take /jointoken.txt file command and hit on worker node to join this cluster.
 fi
-
-
