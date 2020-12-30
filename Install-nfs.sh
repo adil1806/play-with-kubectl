371d2307987438847f06272b8ff03eebf7ee9306
KHAN ADIL <36574088+adil1806@users.noreply.github.com>
2020-12-30 11:30:17 +0530

    Create Install-nfs.sh

---
+++ Install-nfs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Info
+echo -n "[TASK 1] Installing all updates, this may take a few minutes ...";
+apt-get update -y >/dev/null 2>&1
+
+## Install NFS
+echo -n "[TASK 2] Installing NFS ...";
+apt-get install nfs-kernel-server -y >/dev/null 2>&1
+
+
+## NFS Folders
+echo -n "[TASK 3] Creating/Adjusting Folder /nfs-demo and editing /etc/exports ...";
+
+mkdir /nfs-demo >/dev/null 2>&1
+chmod -R 755 /nfs-demo >/dev/null 2>&1
+chown nobody:nogroup /nfs-demo 
+echo "/nfs-demo *(rw,sync,no_root_squash,no_all_squash)" >> /etc/exports
+
+## Restart Services
+echo -n "[TASK 4] Restarting Services ...";
+service nfs-kernel-server restart >/dev/null 2>&1
