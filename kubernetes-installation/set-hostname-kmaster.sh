459ab9f9d3473f6cbb5d8cc2367f47017de35fac
KHAN ADIL <36574088+adil1806@users.noreply.github.com>
2020-12-30 13:33:56 +0530

    Update set-hostname-kmaster.sh

--- kubernetes-installation/set-hostname-kmaster.sh
+++ kubernetes-installation/set-hostname-kmaster.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+############################################################# PLAY WITH KUBECTL #############################################################################
+#       Setting up hostname as kmaster
+#       system compatible: Ubuntu:18.04.5 LTS (Bionic Beaver) and 20.04.1 LTS (Focal Fossa).     
+#       Maintainer: Adil Abdullah Khan  
+#       To Run this script:
+#       curl  https://raw.githubusercontent.com/adil1806/play-with-kubectl/master/kubernetes-installation/set-hostname-kmaster.sh | bash
+#############################################################################################################################################################
 
+#!/bin/bash
 
 echo "[TASK 1] Setting hostname for this node to Kmaster"
 hostnamectl set-hostname kmaster >/dev/null 2>&1
