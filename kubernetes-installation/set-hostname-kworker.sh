e791976207b9cce9f429449c8b9125d9c8ca19d6
KHAN ADIL <36574088+adil1806@users.noreply.github.com>
2020-12-30 13:34:50 +0530

    Update set-hostname-kworker.sh

--- kubernetes-installation/set-hostname-kworker.sh
+++ kubernetes-installation/set-hostname-kworker.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+  
+############################################################# PLAY WITH KUBECTL #############################################################################
+#       Setting up hostname as kworker
+#       system compatible: Ubuntu:18.04.5 LTS (Bionic Beaver) and 20.04.1 LTS (Focal Fossa).     
+#       Maintainer: Adil Abdullah Khan  
+#       To Run this script:
+#       curl  https://raw.githubusercontent.com/adil1806/play-with-kubectl/master/kubernetes-installation/set-hostname-kworker.sh | bash
+#############################################################################################################################################################
 
+#!/bin/bash
 
 echo "[TASK 1] Setting hostname for this node to Kworker"
 hostnamectl set-hostname kworker >/dev/null 2>&1
